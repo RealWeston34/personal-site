5163963093a560f6443e736d2471abd16bac37c9
Scott Weston <scottricardo@ucsb.edu>
2023-12-28 12:37:38 -0800

    fixing linkedin link bug, deploying

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -12,7 +12,7 @@ const Footer = () => {
           <a href="https://github.com/RealWeston34" target="_blank" rel="noopener noreferrer">
             <img src={github} alt="github" className="fab fa-facebook" />
           </a>
-          <a href="https://www.linkedin.com/in/scott-f-641135204/" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.linkedin.com/in/scottyyy/" target="_blank" rel="noopener noreferrer">
             <img src={linkedin} alt="linkedin" className="fab fa-twitter" />
           </a>
         </div>
